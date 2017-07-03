630a124c2824ce389dd1a5380ee2b161a6997033
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-07-03 09:11:22 +0100

    Move chrome alias into correct place

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,5 +18,3 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
-
-alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias gulp='nocorrect gulp'
 alias eslint='nocorrect eslint'
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
